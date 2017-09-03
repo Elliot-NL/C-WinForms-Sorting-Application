48c6ce0e05e1764c017bb3c5425f44c621af4680
Elliot-NL <elliotisvery@hotmail.co.uk>
2017-09-03 13:35:45 +0100

    Add files via upload
    [03/08/17] QuickSort Algorithm complete - uses LINQ to achieve sort however.

---
+++ QuickSortClass.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsForms___Sorting_Algorithms
+{
+    public static class QuickSortClass
+    {
+        public static int piv;
+        public static List<int> QuickList = new List<int>();
+
+        public static void QSort(this List<int> QuickList)
+        {
+            foreach(int items in QuickList)
+            {
+                int total = QuickList.Sum();
+                int length = QuickList.Count;
+                piv = total /= length;
+            }
+            for(int i = 0; i < QuickList.Count; i++)
+            {
+                QuickList.Sort(); //Not QuickSort
+                string result = string.Join(",", QuickList);
+                //Output not implemented here
+            }
+        }
+    }
+}
